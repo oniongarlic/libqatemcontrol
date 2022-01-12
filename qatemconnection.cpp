72eff547b8d7dc5294fbf63b762b85a88dadbc7c
Kaj-Michael Lang <milang@tal.org>
2022-01-12 10:13:48 +0200

    Preliminary disk recording status message parsing

--- qatemconnection.cpp
+++ qatemconnection.cpp
@@ -1032,13 +1032,25 @@ void QAtemConnection::onStRS(const QByteArray& payload)
  */
 void QAtemConnection::onRTMD(const QByteArray& payload)
 {
-    QAtem::U32_U8 val;
-    val.u8[3] = static_cast<quint8>(payload.at(6));
-    val.u8[2] = static_cast<quint8>(payload.at(7));
-    val.u8[1] = static_cast<quint8>(payload.at(8));
-    val.u8[0] = static_cast<quint8>(payload.at(9));
+    QAtem::U32_U8 id;
+    id.u8[3] = static_cast<quint8>(payload.at(6));
+    id.u8[2] = static_cast<quint8>(payload.at(7));
+    id.u8[1] = static_cast<quint8>(payload.at(8));
+    id.u8[0] = static_cast<quint8>(payload.at(9));
 
-    qDebug() << "RTMD" << payload.size() << ":" << val.u32;
+    QAtem::U32_U8 time;
+    time.u8[3] = static_cast<quint8>(payload.at(13));
+    time.u8[2] = static_cast<quint8>(payload.at(12));
+    time.u8[1] = static_cast<quint8>(payload.at(11));
+    time.u8[0] = static_cast<quint8>(payload.at(10));
+
+    QAtem::U16_U8 status;
+    status.u8[1] = static_cast<quint8>(payload.at(15));
+    status.u8[0] = static_cast<quint8>(payload.at(14));
+
+    QString volumename = payload.mid(16, 64);
+
+    qDebug() << "RTMD" << payload.size() << ":" << id.u32 << time.u32 << status.u16 << volumename;
 }
 
 /**
