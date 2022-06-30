2839372e4842ace916c7589b2f9281c777309d99
Kaj-Michael Lang <milang@tal.org>
2022-06-30 12:48:40 +0300

    Add signed 64-bit, add helpers

--- qatemtypes.h
+++ qatemtypes.h
@@ -50,6 +50,48 @@ namespace QAtem
         qint8 s8[4];
     } S32_S8;
 
+    typedef union
+    {
+        qint64 s64;
+        qint8  s8[8];
+    } S64_S8;
+
+    inline quint16 uint16at(const QByteArray &ba, int p) {
+        QAtem::U16_U8 val;
+        val.u8[1] = static_cast<quint8>(ba.at(p));
+        val.u8[0] = static_cast<quint8>(ba.at(p+1));
+
+        return val.u16;
+    };
+
+    inline quint16 uint32at(const QByteArray &ba, int p) {
+        QAtem::U32_U8 val;
+        val.u8[3] = static_cast<quint8>(ba.at(p));
+        val.u8[2] = static_cast<quint8>(ba.at(p+1));
+        val.u8[1] = static_cast<quint8>(ba.at(p+2));
+        val.u8[0] = static_cast<quint8>(ba.at(p+3));
+
+        return val.u32;
+    };
+
+    inline qint64 int64at(const QByteArray &ba, int p) {
+        QAtem::S64_S8 val;
+        val.s8[7] = static_cast<qint8>(ba.at(p));
+        val.s8[6] = static_cast<qint8>(ba.at(p+1));
+        val.s8[5] = static_cast<qint8>(ba.at(p+2));
+        val.s8[4] = static_cast<qint8>(ba.at(p+3));
+        val.s8[3] = static_cast<qint8>(ba.at(p+4));
+        val.s8[2] = static_cast<qint8>(ba.at(p+5));
+        val.s8[1] = static_cast<qint8>(ba.at(p+6));
+        val.s8[0] = static_cast<qint8>(ba.at(p+7));
+
+        return val.s64;
+    };
+
+    inline bool boolat(const QByteArray &ba, int p) {
+        return static_cast<bool>(ba.at(p));
+    };
+
     struct LIBQATEMCONTROLSHARED_EXPORT Topology
     {
         quint8 MEs;
